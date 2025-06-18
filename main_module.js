039af7384be93ff4d7d0a77f9c1c056c6b9028b7
Isaac Wilson <isaacrw248@gmail.com>
2025-06-18 16:16:43 +0100

    Simplified page and made Calc a class

--- main_module.js
+++ main_module.js
@@ -1,75 +1,72 @@
-let operand1 = 0
-let operand2 = 0
-let operator = null
-let checker = undefined
-let display = " "
-let running = false 
-
 const calcDisplay = document.querySelector(".Calculator.Display")
-const onButton = document.getElementById("On_Button")
 
-equalOperator.addEventListener("click", () => {});
 
-function Turn_On() { 
+const Calculator = {
 
+    operand_1 : 0,
+    operand_2 : 0,
+    operator : " ",
+    running : false, 
+    display : "",
+    prevAns : 0,
+
+    Turn_On : function(){ 
+
+    this.running = true
     calcDisplay.style.opacity = "90%"
-    running = true
-}
+    calcDisplay.textContent = this.display },
 
-function ShowDisplay(input) { 
+    Show_Display : function(input){ 
 
-    if ( running===true ) {
+        if ( this.running===true ) {
 
-    display += input
-    calcDisplay.textContent = display
- }
-
-}
-
-function ClearDisplay() {
-
-    display = " "
-    calcDisplay.textContent = display
-
-}
-
-function Deletion() { 
-    display -= input 
-    calcDisplay.textContent = display
-}
-
-Admin = { 
-
-}
-  
-Arithmetic = { 
-
-    Addition : (x,y) => { return (x + y)},
-    Subtraction : (x,y) => { return x - y},
-    Division : (x,y) => { return x/y },
-    Multiplication : (x,y) => { return x*y},
-    /* Equals displays result */
-
-}
-
-
-Lgrthm = { 
-
+        this.display += input
+        calcDisplay.textContent = this.display 
     
-}
+     } 
+    },
 
+    Clear_Display : function(){
 
-Expn = { 
+    this.display = " "
+    calcDisplay.textContent = this.display  
+    
+    this.operand_1 = 0
+    this.operand_2 = 0
+    this.operator = " " 
+ 
+    },
 
-    root_func : (x) => { return Math.sqrt(x)},
-    exp_func : (x,y) => { return x**y},
+    Evaluate : function(){ 
 
-}
+    let display_line = this.display 
 
-Trigmtry = { 
+    const display_line_bits = display_line.split(" ")
 
-    sin_func : (x) => { return Math.sin(x)},
-    cos_func : (x) => { return Math.cos(x)},
-    tan_func : (x) => { return Math.tan(x)},
+    this.operand_1 = display_line_bits[0]
+    this.operator_choice = display_line_bits[1]
+    this.operand_2 = display_line_bits[2]
 
-}
+    if (this.operator_choice === '+') { operator = addFunc() } 
+    else if (this.operator_choice === '-') { operator = subFunc() }
+    else if ( this.operator_choice = '÷') { operator = divFunc() } 
+    else if ( this.operator_choice === 'X') { operator = mulFunc()}
+
+    this.display = operator(parseFloat(this.operand1),parseFloat(this.operand2))
+    calcDisplay.textContent = this.display
+    prevAns = this.display 
+
+    this.operand_1 = 0
+    this.operand_2 = 0
+    this.operator = ""
+
+    },
+
+    addFunc : (x,y) => { return x+y },
+    subFunc : (x,y) => { return x-y},
+    divFunc : (x,y) => { if ( y===0 ) { return "You cant divide by 0"} else { return x/y} },
+    mulFunc : (x,y) => { return x*y},
+    
+    AnsFunc  : () => { calcDisplay.textContent = prevAns},
+    
+};
