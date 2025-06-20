4d283c4df5dca3bd7c6ec76d3d535c9cd2c30f14
Isaac Wilson <isaacrw248@gmail.com>
2025-06-20 11:41:53 +0100

    Added basic functionality ( still error prone - cannot do multiple operations yet )

--- main_module.js
+++ main_module.js
@@ -3,12 +3,18 @@ const calcDisplay = document.querySelector(".Calculator.Display")
 
 const Calculator = {
 
-    operand_1 : 0,
-    operand_2 : 0,
-    operator : " ",
+    operand_1 : "",
+    operand_2 : "",
+    operator : "",
     running : false, 
     display : "",
-    prevAns : 0,
+
+    addFunc : (x,y) => { return x+y },
+    subFunc : (x,y) => { return x-y},
+    divFunc : (x,y) => { if ( y===0 ) { return "You cant divide by 0"} else { return x/y} },
+    mulFunc : (x,y) => { return x*y},
+    
+    AnsFunc  : () => { calcDisplay.textContent = prevAns},
 
     Turn_On : function(){ 
 
@@ -18,55 +24,45 @@ const Calculator = {
 
     Show_Display : function(input){ 
 
-        if ( this.running===true ) {
+        if (this.display != "") { this.display = "", calcDisplay.textContent = this.display}
 
+        if ( this.running===true ) {
         this.display += input
         calcDisplay.textContent = this.display 
-    
      } 
+
+     if (input==='+') { this.operator = this.addFunc } else if (input==='-') { this.operator = this.subFunc }
+     else if (input==='X') { this.operator = this.mulFunc } else if (input==='÷') { this.operator = this.divFunc }
+
     },
 
     Clear_Display : function(){
 
-    this.display = " "
+    this.display = ""
     calcDisplay.textContent = this.display  
     
-    this.operand_1 = 0
-    this.operand_2 = 0
-    this.operator = " " 
+    this.operand_1 = ""
+    this.operand_2 = ""
+    this.operator = "" 
  
     },
 
+    Delete_Display : function() { 
+
+    },
+
+    operandInputFunc : function(number) {
+        if (this.operator === "") { this.operand_1 += number} else { this.operand_2 += number }
+     },
+
     Evaluate : function(){ 
 
-    let display_line = this.display 
-
-    const display_line_bits = display_line.split(" ")
-
-    this.operand_1 = display_line_bits[0]
-    this.operator_choice = display_line_bits[1]
-    this.operand_2 = display_line_bits[2]
-
-    if (this.operator_choice === '+') { operator = addFunc() } 
-    else if (this.operator_choice === '-') { operator = subFunc() }
-    else if ( this.operator_choice = '÷') { operator = divFunc() } 
-    else if ( this.operator_choice === 'X') { operator = mulFunc()}
-
-    this.display = operator(parseFloat(this.operand1),parseFloat(this.operand2))
+    this.display = this.operator(parseFloat(this.operand_1),parseFloat(this.operand_2))
     calcDisplay.textContent = this.display
-    prevAns = this.display 
 
     this.operand_1 = 0
     this.operand_2 = 0
     this.operator = ""
 
     },
-
-    addFunc : (x,y) => { return x+y },
-    subFunc : (x,y) => { return x-y},
-    divFunc : (x,y) => { if ( y===0 ) { return "You cant divide by 0"} else { return x/y} },
-    mulFunc : (x,y) => { return x*y},
-    
-    AnsFunc  : () => { calcDisplay.textContent = prevAns},
-    
 };
