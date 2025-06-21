7c7126ea8b91e71869d89912e3b42135ff36a653
Isaac Wilson <isaacrw248@gmail.com>
2025-06-21 17:01:33 +0100

    Added full functionality apart from DEL

--- main_module.js
+++ main_module.js
@@ -1,21 +1,20 @@
 const calcDisplay = document.querySelector(".Calculator.Display")
 
-
 const Calculator = {
 
     operand_1 : "",
     operand_2 : "",
     operator : "",
-    running : false, 
     display : "",
+    prev_ans : "",
+    op_clicked : false, 
+    running : false, 
 
     addFunc : (x,y) => { return x+y },
     subFunc : (x,y) => { return x-y},
     divFunc : (x,y) => { if ( y===0 ) { return "You cant divide by 0"} else { return x/y} },
     mulFunc : (x,y) => { return x*y},
     
-    AnsFunc  : () => { calcDisplay.textContent = prevAns},
-
     Turn_On : function(){ 
 
     this.running = true
@@ -24,16 +23,22 @@ const Calculator = {
 
     Show_Display : function(input){ 
 
-        if (this.display != "") { this.display = "", calcDisplay.textContent = this.display}
+        if ( this.running === true ) {
 
-        if ( this.running===true ) {
+        if (this.op_clicked) {this.display = "", calcDisplay.textContent = this.display } 
+        else { this.display = "", calcDisplay.textContent = this.display }
+
         this.display += input
         calcDisplay.textContent = this.display 
-     } 
 
-     if (input==='+') { this.operator = this.addFunc } else if (input==='-') { this.operator = this.subFunc }
-     else if (input==='X') { this.operator = this.mulFunc } else if (input==='÷') { this.operator = this.divFunc }
+        if (this.operator === "") { this.operand_1 += input } else { this.operand_2 += input }
 
+        if (input==='+') { this.operator = this.addFunc, this.op_clicked = true } 
+        else if (input==='-') { this.operator = this.subFunc, this.op_clicked = true }
+        else if (input==='X') { this.operator = this.mulFunc, this.op_clicked = true } 
+        else if (input==='÷') { this.operator = this.divFunc, this.op_clicked = true }
+
+        }
     },
 
     Clear_Display : function(){
@@ -44,24 +49,25 @@ const Calculator = {
     this.operand_1 = ""
     this.operand_2 = ""
     this.operator = "" 
+    this.op_clicked = false 
  
     },
 
     Delete_Display : function() { 
 
-    },
+    if (this.operator === "") { this.operand_1 -= number, this.display -= input, calcDisplay.textContent = this.display}  
+    else { this.operand_2 -= number, this.display -= input, calcDisplay.textContent = this.display }
 
-    operandInputFunc : function(number) {
-        if (this.operator === "") { this.operand_1 += number} else { this.operand_2 += number }
-     },
+    },
 
     Evaluate : function(){ 
 
-    this.display = this.operator(parseFloat(this.operand_1),parseFloat(this.operand_2))
+    z = this.operator(parseFloat(this.operand_1),parseFloat(this.operand_2))
+    this.display = z 
     calcDisplay.textContent = this.display
 
-    this.operand_1 = 0
-    this.operand_2 = 0
+    this.operand_1 = z  
+    this.operand_2 = ""
     this.operator = ""
 
     },
